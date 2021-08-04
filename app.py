b40e14207d7272ef7bbdac1e3fb35e49e5599a11
yuvalgrin <juvs2k@gmail.com>
2021-08-04 15:38:07 +0300

    html updates

--- app.py
+++ app.py
@@ -13,13 +13,27 @@ db = SQLAlchemy(app)
 class Cable(db.Model):
     __tablename__ = 'cables'
     product_number = db.Column(db.Integer, primary_key=True)
-    family = db.Column(db.Integer, unique=True, nullable=False)
-    frequency = db.Column(db.Integer, unique=True, nullable=False)
-    diameter = db.Column(db.Integer, unique=True, nullable=False)
+    family = db.Column(db.Integer, unique=False, nullable=False)
+    frequency = db.Column(db.Integer, unique=False, nullable=False)
+    diameter = db.Column(db.Integer, unique=False, nullable=False)
 
     def __repr__(self):
-        return "{\"product_number\":" + str(self.product_number) +\
-               ",\"frequency\":" + str(self.frequency) + "}"
+        return "{\"product_number\":\"" + str(self.product_number) + "\"" +\
+               ",\"family\":" + str(self.family) +\
+               ",\"frequency\":" + str(self.frequency) +\
+               ",\"diameter\":" + str(self.diameter) +\
+               + "}"
+
+
+class Connector(db.Model):
+    __tablename__ = 'connectors'
+    var1 = db.Column(db.Integer, primary_key=True)
+    var2 = db.Column(db.Integer, unique=True, nullable=False)
+
+    def __repr__(self):
+        return "{\"var1\":\"" + str(self.var1) + "\"" +\
+               ",\"var2\":" + str(self.var2) +\
+               + "}"
 
 
 @app.route("/")
@@ -28,41 +42,26 @@ def home():
     return render_template('main.html')
 
 
-@app.route("/filter")
-def filer():
+@app.route("/filter-cable")
+def filer_cable():
+    # Get all the parameter from the url (?length=10&freq....)
     length = request.args.get('length')
     frequency = request.args.get('frequency')
     diameter = request.args.get('diameter')
-    cables = Cable.query.all()
-    # cables = Cable.query.filter_by(Cable.frequency > frequency, Cable.diameter == diameter).first()
-
-    # result = db.engine.execute(text("SELECT * FROM cables;").execution_options(autocommit=True))
-    # result = Cable.query.filter_by(length=length).first()
-    # return str(result);
-    return str(cables);
-    # return json.loads(to_json(cables, Cable))
+    # cables = Cable.query.all()
+    query_result = Cable.query.filter_by(Cable.frequency > frequency, Cable.diameter == diameter)
+    return str(query_result)
 
 
-def to_json(inst, cls):
-    """
-    Jsonify the sql alchemy query result.
-    """
-    convert = dict()
-    # add your coversions for things like datetime's
-    # and what-not that aren't serializable.
-    d = dict()
-    for c in cls.__table__.columns:
-        v = getattr(inst, c.name)
-        if c.type in convert.keys() and v is not None:
-            try:
-                d[c.name] = convert[c.type](v)
-            except:
-                d[c.name] = "Error:  Failed to covert using ", str(convert[c.type])
-        elif v is None:
-            d[c.name] = str()
-        else:
-            d[c.name] = v
-    return json.dumps(d)
+
+@app.route("/filter-connector")
+def filer_connector():
+    var1 = request.args.get('var1')
+    var2 = request.args.get('var2')
+    # cables = Cable.query.all()
+    query_result = Connector.query.filter_by(Connector.var1 > var1, Connector.var2 == var2).first()
+    return str(query_result)
+
 
 
 if __name__ == '__main__':
